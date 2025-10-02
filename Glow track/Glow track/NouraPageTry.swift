fccda19068c514c94de9cd407d13a1eb86ef548f
n <n>
2025-10-02 17:16:57 +0300

    New File

---
+++ Glow track/Glow track/NouraPageTry.swift
@@ -0,0 +1,18 @@
+//
+//  NouraPageTry.swift
+//  Glow track
+//
+//  Created by Noura Alsmari on 02/10/2025.
+//
+
+import SwiftUI
+
+struct NouraPageTry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NouraPageTry()
+}
